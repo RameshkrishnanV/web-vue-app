691c9795ffc56af682cdd960fa7c3303f6c47577
Ramesh <rsramesh572@gmail.com>
2021-01-11 19:16:58 +0530

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+html, body, #fullheight {
+    min-height: 100% !important;
+    height: 100%;
+    padding: 0; margin: 0;
+      background-image: url("./assets/home_bg.png");
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+</style>
